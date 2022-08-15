a5740fba1c89067594b81aae58dd80ba20eba241
HowePersonal <howe2004new@gmail.com>
2022-08-15 19:34:43 -0400

    Made function to decide result based on user input

--- code.js
+++ code.js
@@ -11,6 +11,24 @@ let getComputerChoice = () => {
 
 }
 
-let start = () => {
-    
+let start = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+
+    if (playerSelection === computerSelection) {
+        return `Tied! Both used ${computerSelection}`
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return `You lose! Rock is beaten by Paper!`
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return `You win! Rock beats Scissors!`
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return `You win! Paper beats Rock!`
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return `You lose! Paper is beaten by Scissors!`
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return `You lose! Scissor is beaten by Rock!`
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return `You win! Scissor beats Paper!`
+    } 
+
+    return `${playerSelection} is not a valid input`
 }
