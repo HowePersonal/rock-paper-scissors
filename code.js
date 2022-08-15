6dc05481b49269ad5e148d7f0adadf60f375fc40
HowePersonal <howe2004new@gmail.com>
2022-08-15 19:22:08 -0400

    Made js function to randomly choose R/P/S

---
+++ code.js
@@ -0,0 +1,16 @@
+let getComputerChoice = () => {
+    let randomNum = Math.floor(Math.random() * 3)
+    
+    if (randomNum === 0) {
+        return 'Rock'
+    } else if (randomNum === 1) {
+        return 'Paper'
+    } else if (randomNum === 2) {
+        return 'Scissors'
+    }
+
+}
+
+let start = () => {
+    
+}
